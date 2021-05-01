ef2affe927da94cf85906fb2af67605e39203450
Iratu <me@iratu.pro>
2021-05-01 20:12:11 +0000

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,106 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import babel from '@rollup/plugin-babel'
+import filesize from 'rollup-plugin-filesize'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
+import size from 'rollup-plugin-sizes'
+import { terser } from 'rollup-plugin-terser'
+import visualize from 'rollup-plugin-visualizer'
+import { version as clapprCoreVersion } from '@clappr/core/package.json'
+import pkg from './package.json'
+
+let rollupConfig
+
+const serveLocal = !!process.env.SERVE
+const reloadEnabled = !!process.env.RELOAD
+const analyzeBundle = !!process.env.ANALYZE_BUNDLE
+const minimize = !!process.env.MINIMIZE
+
+const babelOptionsPlugins = { exclude: 'node_modules/**', babelHelpers: 'bundled' }
+const servePluginOptions = { contentBase: ['dist', 'public'], host: '0.0.0.0', port: '8080' }
+const livereloadPluginOptions = { watch: ['dist', 'public'] }
+const baseReplacePluginOptions = { CLAPPR_CORE_VERSION: JSON.stringify(clapprCoreVersion) }
+const replacePluginOptions = { ...baseReplacePluginOptions, 'process.env.NODE_ENV': JSON.stringify('development') }
+
+let plugins = [
+  replace(baseReplacePluginOptions),
+  resolve(),
+  commonjs(),
+  babel(babelOptionsPlugins),
+  size(),
+  filesize(),
+]
+
+serveLocal && (plugins = [...plugins, replace(replacePluginOptions), serve(servePluginOptions)])
+reloadEnabled && (plugins = [...plugins, livereload(livereloadPluginOptions)])
+analyzeBundle && plugins.push(visualize({ open: true }))
+
+const mainBundle = {
+  external: ['@clappr/core'],
+  input: 'src/main.js',
+  output: {
+    name: 'MpegtsJSPlayback',
+    file: pkg.main,
+    format: 'umd',
+    globals: { '@clappr/core': 'Clappr' },
+  },
+  plugins,
+}
+
+const mainBundleWithoutHLS = {
+  external: ['@clappr/core', 'mpegts.js'],
+  input: 'src/main.js',
+  output: {
+    name: 'MpegtsJSPlayback',
+    file: 'dist/clappr-mpegts-playback.external.js',
+    format: 'umd',
+    globals: { '@clappr/core': 'Clappr', 'mpegts.js': 'Hls' },
+  },
+  plugins,
+}
+
+const mainBundleMinified = {
+  input: 'src/main.js',
+  output: {
+    name: 'MpegtsJSPlayback',
+    file: 'dist/clappr-mpegts-playback.min.js',
+    format: 'iife',
+    sourcemap: true,
+    plugins: terser(),
+  },
+  plugins,
+}
+
+const mainBundleWithoutHLSMinified = {
+  external: ['@clappr/core', 'mpegts.js'],
+  input: 'src/main.js',
+  output: {
+    name: 'MpegtsJSPlayback',
+    file: 'dist/clappr-mpegts-playback.external.min.js',
+    globals: { '@clappr/core': 'Clappr', 'mpegts.js': 'Hls' },
+    format: 'iife',
+    sourcemap: true,
+    plugins: terser(),
+  },
+  plugins,
+}
+
+const moduleBundle = {
+  external: ['@clappr/core'],
+  input: 'src/main.js',
+  output: {
+    name: 'MpegtsJSPlayback',
+    file: pkg.module,
+    format: 'esm',
+    globals: { '@clappr/core': 'Clappr' },
+  },
+  plugins,
+}
+
+rollupConfig = [mainBundle, mainBundleWithoutHLS, moduleBundle]
+serveLocal && (rollupConfig = [mainBundle, mainBundleWithoutHLS])
+minimize && rollupConfig.push(mainBundleMinified, mainBundleWithoutHLSMinified)
+
+export default rollupConfig
---
+++ src/main.js
@@ -0,0 +1,98 @@
+import { Events, HTML5Video, Log, Playback, PlayerError } from '@clappr/core'
+import mpegts from 'mpegts.js'
+
+const MIMETYPES = ['video/flv', 'video/x-flv']
+const EXTENSION = 'flv'
+
+export default class MpegtsJSPlayback extends HTML5Video {
+  get name () {
+    return 'mpegts'
+  }
+
+  get supportedVersion () {
+    // eslint-disable-next-line no-undef
+    return { min: CLAPPR_CORE_VERSION }
+  }
+
+  get isHTML5Video () {
+    return true
+  }
+
+  get mpegts () {
+    return this._player
+  }
+
+  getPlaybackType () {
+    return this._playbackType
+  }
+
+  play () {
+    !this._player && this._setup()
+    super.play()
+  }
+
+  stop () {
+    super.stop()
+    this._destroy()
+  }
+
+  destroy () {
+    this._destroy()
+    super.destroy()
+  }
+
+  // skipping setup `setupSrc` on tag video
+  _setupSrc () { }
+
+  _setup () {
+    this._destroy()
+
+    const mediaDataSource = {
+      type: EXTENSION,
+      url: this.options.src
+    }
+    const mpegtsConfig = this.options.playback.mpegtsConfig || {}
+    this._playbackType = mpegtsConfig.isLive ? Playback.LIVE : Playback.VOD
+
+    const enableLogging = mpegtsConfig.enableLogging || false
+    mpegts.LoggingControl.enableAll = enableLogging
+
+    this._player = mpegts.createPlayer(mediaDataSource, mpegtsConfig)
+    this._player.on(mpegts.Events.ERROR, this._onError.bind(this))
+    this._player.attachMediaElement(this.el)
+    this._player.load()
+  }
+
+  _onError (type, details, data) {
+    Log.error(`mpegts: ${type}: ${details}`, data)
+
+    const formattedError = this.createError({
+      code: data.code || type,
+      description: data.msg || details,
+      raw: data,
+      level: PlayerError.Levels.FATAL
+    })
+
+    this.trigger(Events.PLAYBACK_ERROR, formattedError)
+    this.stop()
+  }
+
+  _destroy () {
+    if (!this._player) {
+      return
+    }
+
+    this._player.destroy()
+    delete this._player
+  }
+}
+
+MpegtsJSPlayback.canPlay = (resource, mimeType = '') => {
+  const resourceParts = (resource.split('?')[0].match(/.*\.(.*)$/) || [])
+  const isFLV = ((resourceParts.length > 1 && resourceParts[1].toLowerCase() === EXTENSION) ||
+    MIMETYPES.indexOf(mimeType) !== -1)
+  return mpegts.isSupported() && isFLV
+}
+
+// eslint-disable-next-line no-undef
+MpegtsJSPlayback.version = () => VERSION
